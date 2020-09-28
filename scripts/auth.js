643f7d84a2500377bfa1ae858541762133306aad
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-09-28 18:26:40 +0530

    Added info on the rules stored in firestore for reference

--- scripts/auth.js
+++ scripts/auth.js
@@ -108,3 +108,24 @@ loginForm.addEventListener("submit", (e) => {
       loginForm.querySelector(".error").innerHTML = err.message;
     });
 });
+
+// Firebase Firestore Rules
+// rules_version = '2';
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//     // match /{document=**} {
+//     //   allow read, write: if
+//     //       request.time < timestamp.date(2020, 10, 28);
+//     // }
+//     // match docs in the guides collection
+//     match /users/{userId} {
+//     	allow create: if request.auth.uid != null
+//       allow read: if request.auth.uid == userId
+//     }
+//     // match docs in the guides collection
+//     match /guides/{guideId} {
+//     	allow read: if request.auth.uid != null
+//       allow write: if request.auth.token.admin == true
+//     }
+//   }
+// }
